e66ac59b6da08a7fb850c4939c936e0c0ecbefc4
B Steele <B.Steele@colostate.edu>
2024-10-08 10:33:37 -0600

    add herbie pipeline and env setup

---
+++ get_forecasts/herbie_py_setup.R
@@ -0,0 +1,25 @@
+# this script installs python and creates a python environment to run the Herbie
+# downloads using RStudio
+
+library(reticulate)
+
+# activate conda env or create conda env and modules for the venv
+tryCatch(use_condaenv(file.path(getwd(), 'herbie_env')),
+         warning = function(w){
+           print("conda environment activated")
+         },
+         error = function(e) {
+           # install miniconda if necessary
+           try(install_miniconda())
+           #create a conda environment named 'mod_env' with the packages you need
+           conda_create(envname = file.path(getwd(), 'herbie_env'), 
+                        python_version = 3.11)
+           conda_install(envname = 'herbie_env/', 
+                         packages = c('pandas', 'herbie-data', 'matplotlib', 
+                                      'toolbox', 'numpy', 'xarray'))
+           # set the new python environment
+           use_condaenv(file.path(getwd(), "herbie_env/"))
+           print("conda environment created and activated")
+         }
+)
+
