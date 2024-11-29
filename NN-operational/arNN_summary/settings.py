cb9bd729e65f2084e9b1cad30f18713df4003083
B Steele <B.Steele@colostate.edu>
2024-11-29 16:54:05 -0700

    try daily summ not 3h

---
+++ NN-operational/arNN_summary/settings.py
@@ -0,0 +1,32 @@
+settings = {
+  "leaky_basic_5" : {
+    "hiddens": [10, 10],
+    "activations": ["leaky_relu", "leaky_relu"],
+    "learning_rate": 0.001,
+    "random_seed": 57,
+    "max_epochs": 1000,
+    "batch_size": 64,
+    "patience": 200,
+    "dropout_rate": 0.1
+  },
+  "leaky_5_do2" : {
+    "hiddens": [10, 10],
+    "activations": ["leaky_relu", "leaky_relu"],
+    "learning_rate": 0.001,
+    "random_seed": 57,
+    "max_epochs": 1000,
+    "batch_size": 64,
+    "patience": 200,
+    "dropout_rate": 0.2
+  },
+  "leaky_5_batch" : {
+    "hiddens": [10, 10],
+    "activations": ["leaky_relu", "leaky_relu"],
+    "learning_rate": 0.001,
+    "random_seed": 57,
+    "max_epochs": 1000,
+    "batch_size": 128,
+    "patience": 200,
+    "dropout_rate": 0.2
+  }
+}
